7252aa40ba95352dac0322cc35a5692dbee0715c
Christopher Brognara <c.brognara@abraon.com>
2022-12-12 15:12:01 +0100

    Added file dialogs

--- Cibbi.CFAM/Services/DialogProvider.cs
+++ Cibbi.CFAM/Services/DialogProvider.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia;
+﻿using System.Linq.Expressions;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Cibbi.CFAM.ViewModels;
@@ -18,7 +19,7 @@ public class DialogProvider
 {
     private readonly IViewLocator _locator = AvaloniaLocator.Current.GetRequiredService<IViewLocator>();
     
-    public async Task<string?> ShowFolderDialogAsync(string title)
+    public async Task<string?> ShowOpenFolderDialogAsync(string title)
     {
         if (Application.Current!.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime
             applicationLifetime2)
@@ -36,6 +37,64 @@ public class DialogProvider
         var dialog = new OpenFolderDialog
         {
             Title = title
+        };
+
+        return await dialog.ShowAsync(window1);
+
+    }
+    
+    public async Task<string[]?> ShowOpenFileDialogAsync(string title, string? directory = null, string? initialFileName = null, 
+        List<FileDialogFilter>? filters = null, bool allowMultiple = false)
+    {
+        if (Application.Current!.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime
+            applicationLifetime2)
+            throw new NotSupportedException("The file dialog is not supported in non desktop applications");
+        
+        Window? window1 = null;
+        foreach (var window2 in applicationLifetime2.Windows)
+        {
+            if (!window2.IsActive) continue;
+            window1 = window2;
+            break;
+        }
+        window1 ??= applicationLifetime2.MainWindow;
+        
+        var dialog = new OpenFileDialog
+        {
+            Title = title,
+            Filters = filters,
+            Directory = directory,
+            InitialFileName = initialFileName,
+            AllowMultiple = allowMultiple
+        };
+
+        return await dialog.ShowAsync(window1);
+
+    }
+    
+    public async Task<string?> ShowSaveFileDialogAsync(string title, string? directory = null, string? defaultExtension = null, string? initialFileName = null,
+        List<FileDialogFilter>? filters = null)
+    {
+        if (Application.Current!.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime
+            applicationLifetime2)
+            throw new NotSupportedException("The file dialog is not supported in non desktop applications");
+        
+        Window? window1 = null;
+        foreach (var window2 in applicationLifetime2.Windows)
+        {
+            if (!window2.IsActive) continue;
+            window1 = window2;
+            break;
+        }
+        window1 ??= applicationLifetime2.MainWindow;
+        
+        var dialog = new SaveFileDialog()
+        {
+            Title = title,
+            Filters = filters,
+            Directory = directory,
+            DefaultExtension = defaultExtension,
+            InitialFileName = initialFileName
         };
 
         return await dialog.ShowAsync(window1);
