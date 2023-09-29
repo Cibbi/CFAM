55638c0691356ff86204c1dd1fc54571e5a2d52b
Cibbi <12004047+Cibbi@users.noreply.github.com>
2023-09-29 15:06:28 +0200

    include assembly name in research of the viewModel

--- Cibbi.CFAM/BaseViewLocator.cs
+++ Cibbi.CFAM/BaseViewLocator.cs
@@ -7,13 +7,13 @@ public class BaseViewLocator : IViewLocator
 {
     private static readonly Dictionary<Type, Type?> _viewModelToViewMapping = new();
         
-    public IViewFor ResolveView<T>(T viewModel, string? contract = null)
+    public virtual IViewFor ResolveView<T>(T viewModel, string? contract = null)
     {
         Type? vmType = viewModel?.GetType();
         if (vmType is null) throw new ArgumentNullException(nameof(viewModel));
         if(_viewModelToViewMapping.TryGetValue(vmType, out var vType)) return (IViewFor)Activator.CreateInstance(vType!)!;
 
-        string? name = vmType.FullName!.Replace("ViewModel", "View");
+        string? name = vmType.AssemblyQualifiedName!.Replace("ViewModel", "View");
         if(name is null) throw new ArgumentOutOfRangeException(nameof(viewModel));
         var type = Type.GetType(name);
         if (type is null || !type.IsAssignableTo(typeof(IViewFor)))
