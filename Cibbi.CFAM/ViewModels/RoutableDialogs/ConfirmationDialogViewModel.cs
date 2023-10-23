77cbe45354eebd4cab3feea6ea2594ab89beff65
Cibbi <12004047+Cibbi@users.noreply.github.com>
2023-10-23 12:52:48 +0200

    Added responsivePanel, ConfirmationDialog, and new icon

---
+++ Cibbi.CFAM/ViewModels/RoutableDialogs/ConfirmationDialogViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Reactive;
+using ReactiveUI;
+
+namespace Cibbi.CFAM.ViewModels.RoutableDialogs;
+
+public class ConfirmationDialogViewModel : RoutableViewModel
+{
+    public override string UrlPathSegment => "confirmation";
+    
+    public required string Title { get; init; } 
+    public required string Message { get; init; }
+    
+    public string ConfirmText { get; set; } = "Conferma";
+    public string CancelText { get; set; } = "Annulla";
+    
+    public ReactiveCommand<Unit, Unit> ConfirmCommand { get; }
+    public ReactiveCommand<Unit, Unit> CancelCommand { get; }
+    
+    public ConfirmationDialogViewModel(IScreen screen, ReactiveCommand<Unit, Unit> confirmCommand) : base(screen)
+    {
+        ConfirmCommand = confirmCommand;
+        CancelCommand = ReactiveCommand.Create(DefaultCancel);
+    }
+    
+    public ConfirmationDialogViewModel(IScreen screen, ReactiveCommand<Unit, Unit> confirmCommand, ReactiveCommand<Unit, Unit> cancelCommand) : base(screen)
+    {
+        ConfirmCommand = confirmCommand;
+        CancelCommand = cancelCommand;
+    }
+    
+    private void DefaultCancel()
+    {
+        HostScreen.Router.NavigateBack.Execute();
+    }
+}
---
+++ Cibbi.CFAM/Views/RoutableDialogs/ConfirmationDialogView.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Cibbi.CFAM.ViewModels.RoutableDialogs;
+
+namespace Cibbi.CFAM.Views.RoutableDialogs;
+
+public partial class ConfirmationDialogView : CFAMUserControl<ConfirmationDialogViewModel>
+{
+    public ConfirmationDialogView()
+    {
+        InitializeComponent();
+    }
+}
