a58e7c907901c130fe8eae08b63390e256807a5a
Cibbi <12004047+Cibbi@users.noreply.github.com>
2023-11-20 10:43:20 +0100

    Update DialogsOverlayView to manually handle dialog changes

--- Cibbi.CFAM/Views/Dialogs/DialogsOverlayView.axaml.cs
+++ Cibbi.CFAM/Views/Dialogs/DialogsOverlayView.axaml.cs
@@ -1,7 +1,10 @@
-﻿using Avalonia;
+﻿using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Cibbi.CFAM.ViewModels.Dialogs;
+using ReactiveUI;
 
 namespace Cibbi.CFAM.Views.Dialogs;
 
@@ -10,5 +13,21 @@ public partial class DialogsOverlayView : CFAMUserControl<DialogsOverlayViewMode
     public DialogsOverlayView()
     {
         InitializeComponent();
+        this.WhenActivated((CompositeDisposable disposable) =>
+        {
+            if(ViewModel is null) return;
+            
+            ViewModel.WhenAnyValue(x => x.CurrentDialog)
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Subscribe(x =>
+                {
+                    DialogContent.Children.Clear();
+                    if (x is null) return;
+
+                    if (CFAMSettings.ViewLocator.ResolveView(x) is not Control control) return;
+                    control.DataContext = x;
+                    DialogContent.Children.Add(control);
+                }).DisposeWith(disposable);
+        });
     }
 }
