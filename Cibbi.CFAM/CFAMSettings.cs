5299f673a906dac8f7e3cbe7dbb133c55f98c94a
Cibbi <12004047+Cibbi@users.noreply.github.com>
2023-11-20 14:34:15 +0100

    Add scalable UI
    This modification aims to enhance the adaptability of the application across various screen sizes by exposing a UIScaling global value.

--- Cibbi.CFAM/CFAMSettings.cs
+++ Cibbi.CFAM/CFAMSettings.cs
@@ -8,6 +8,7 @@ public static class CFAMSettings
 {
     public static NotificationPosition NotificationPosition { get; set; } = NotificationPosition.BottomRight;
     public static int MaxNotifications { get; set; } = 5;
+    public static double UIScaling { get; set; } = 1;
     
     public static IViewLocator ViewLocator { get; set; } = new BaseViewLocator();
 }
