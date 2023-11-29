28a7eb7319b731fb6ad3337e041deb78ad779336
Cibbi <12004047+Cibbi@users.noreply.github.com>
2023-11-29 10:02:28 +0100

    Added ReactiveExecution static class

---
+++ Cibbi.CFAM/Extensions/ReactiveExecution.cs
@@ -0,0 +1,27 @@
+﻿using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using JetBrains.Annotations;
+using ReactiveUI;
+
+namespace Cibbi.CFAM.Extensions;
+
+public static class ReactiveExecution
+{
+    public static IObservable<Unit> ExecuteOnThreadAsync(Action action, IScheduler scheduler)
+    {
+        return Observable.Start(action, scheduler);
+    }
+    
+    [UsedImplicitly]
+    public static IObservable<Unit>ExecuteOnUIThreadAsync(Action action)
+    {
+        return ExecuteOnThreadAsync(action, RxApp.MainThreadScheduler);
+    }
+    
+    [UsedImplicitly]
+    public static IObservable<Unit> ExecuteOnTaskThreadAsync(Action action)
+    {
+        return ExecuteOnThreadAsync(action, RxApp.TaskpoolScheduler);
+    }
+}
