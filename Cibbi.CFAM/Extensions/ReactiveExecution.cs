0b7788f98759b93aa57132985e1fd4fb7216add7
Cibbi <12004047+Cibbi@users.noreply.github.com>
2024-03-29 11:12:16 +0100

    Updated ExecuteOnThreadAsync

--- Cibbi.CFAM/Extensions/ReactiveExecution.cs
+++ Cibbi.CFAM/Extensions/ReactiveExecution.cs
@@ -13,19 +13,30 @@ public static class ReactiveExecution
         return Observable.Start(action, scheduler);
     }
     
-    public static IObservable<TResult> ExecuteOnThreadAsync<TResult>(Func<TResult> action, IScheduler scheduler)
+    public static IObservable<TResult> ExecuteOnThreadAsync<TResult>(Func<Task<TResult>> action, IScheduler scheduler)
     {
-        return Observable.Start(action, scheduler);
+        return Observable.StartAsync(action, scheduler);
+    }
+    
+    public static IObservable<Unit> ExecuteOnThreadAsync(Func<Task> action, IScheduler scheduler)
+    {
+        return Observable.StartAsync(action, scheduler);
     }
     
     [PublicAPI]
-    public static IObservable<Unit>ExecuteOnUIThreadAsync(Action action)
+    public static IObservable<Unit> ExecuteOnUIThreadAsync(Action action)
     {
         return ExecuteOnThreadAsync(action, RxApp.MainThreadScheduler);
     }
     
     [PublicAPI]
-    public static IObservable<TResult>ExecuteOnUIThreadAsync<TResult>(Func<TResult> action)
+    public static IObservable<TResult> ExecuteOnUIThreadAsync<TResult>(Func<Task<TResult>>  action)
+    {
+        return ExecuteOnThreadAsync(action, RxApp.MainThreadScheduler);
+    }
+    
+    [PublicAPI]
+    public static IObservable<Unit> ExecuteOnUIThreadAsync(Func<Task>  action)
     {
         return ExecuteOnThreadAsync(action, RxApp.MainThreadScheduler);
     }
@@ -37,7 +48,13 @@ public static class ReactiveExecution
     }
     
     [PublicAPI]
-    public static IObservable<TResult> ExecuteOnTaskThreadAsync<TResult>(Func<TResult> action)
+    public static IObservable<TResult> ExecuteOnTaskThreadAsync<TResult>(Func<Task<TResult>>  action)
+    {
+        return ExecuteOnThreadAsync(action, RxApp.TaskpoolScheduler);
+    }
+    
+    [PublicAPI]
+    public static IObservable<Unit> ExecuteOnTaskThreadAsync(Func<Task>  action)
     {
         return ExecuteOnThreadAsync(action, RxApp.TaskpoolScheduler);
     }
