755c44343c51681c5029b1b1453969f06837ef44
Cibbi <12004047+Cibbi@users.noreply.github.com>
2024-03-29 10:14:23 +0100

    Added func version of ExecuteOnThread

--- Cibbi.CFAM/Extensions/ReactiveExecution.cs
+++ Cibbi.CFAM/Extensions/ReactiveExecution.cs
@@ -13,14 +13,31 @@ public static class ReactiveExecution
         return Observable.Start(action, scheduler);
     }
     
-    [UsedImplicitly]
+    public static IObservable<TResult> ExecuteOnThreadAsync<TResult>(Func<TResult> action, IScheduler scheduler)
+    {
+        return Observable.Start(action, scheduler);
+    }
+    
+    [PublicAPI]
     public static IObservable<Unit>ExecuteOnUIThreadAsync(Action action)
     {
         return ExecuteOnThreadAsync(action, RxApp.MainThreadScheduler);
     }
     
-    [UsedImplicitly]
+    [PublicAPI]
+    public static IObservable<TResult>ExecuteOnUIThreadAsync<TResult>(Func<TResult> action)
+    {
+        return ExecuteOnThreadAsync(action, RxApp.MainThreadScheduler);
+    }
+    
+    [PublicAPI]
     public static IObservable<Unit> ExecuteOnTaskThreadAsync(Action action)
+    {
+        return ExecuteOnThreadAsync(action, RxApp.TaskpoolScheduler);
+    }
+    
+    [PublicAPI]
+    public static IObservable<TResult> ExecuteOnTaskThreadAsync<TResult>(Func<TResult> action)
     {
         return ExecuteOnThreadAsync(action, RxApp.TaskpoolScheduler);
     }
