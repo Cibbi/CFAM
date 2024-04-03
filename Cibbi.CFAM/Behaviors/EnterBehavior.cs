04c3ceac58735f506c12d432132adb766f19b649
Cibbi <12004047+Cibbi@users.noreply.github.com>
2024-04-03 09:47:33 +0200

    Add behaviors and triggers

---
+++ Cibbi.CFAM/Behaviors/EnterBehavior.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Xaml.Interactivity;
+
+namespace Cibbi.CFAM.Behaviors;
+
+public class EnterBehavior : Trigger<TextBox>
+{
+    protected override void OnAttachedToVisualTree()
+    {
+        if (AssociatedObject is null) return;
+        AssociatedObject.KeyDown += KeyDown;
+    }
+    
+    protected override void OnDetachedFromVisualTree()
+    {
+        if (AssociatedObject is null) return;
+        AssociatedObject.KeyDown -= KeyDown;
+    }
+    
+    private void KeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            Interaction.ExecuteActions(AssociatedObject, Actions, e);
+        }
+    }
+}
