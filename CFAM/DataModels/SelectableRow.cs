f577cb5a6a719fcb4a82c590d0633d74060cc494
Cibbi <12004047+Cibbi@users.noreply.github.com>
2024-06-06 14:00:49 +0200

    Added SelectableRow object

---
+++ CFAM/DataModels/SelectableRow.cs
@@ -0,0 +1,15 @@
+﻿using CFAM.ViewModels;
+using PropertyChanged.SourceGenerator;
+
+namespace CFAM.DataModels;
+
+public class SelectableRow<T> : ViewModelBase
+{
+    [Notify] private T _item;
+    [Notify] private bool _isSelected;
+
+    public SelectableRow(T item)
+    {
+        _item = item;
+    }
+}
